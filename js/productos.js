eeaa7cc7cd3569dabbd2a2ebf1f9c54fdba9fb61
Eduardo <eduardomoyla@gmail.com>
2024-09-11 18:30:11 -0400

    error appendchild

--- js/productos.js
+++ js/productos.js
@@ -1,19 +1,25 @@
-// let conteiner = document.querySelectorAll(".text-card")
+const conteiner = document.querySelectorAll(".text-card")
 
-// console.log(conteiner)
-
-fetch('../db/productos.json')
-.then(resultado => resultado.json())
-.then(data => console.table(data))
-
-// fetch("../db/productos.json")
+// fetch('../db/productos.json')
 // .then(resultado => resultado.json())
-// .then(data => {
-//     data.forEach(producto => {
-//         let div = document.createElement('div')
-//         div.innerHTML = `<h3 id="${producto.id}"> ${producto.nombre} </h3>
-//                         <p> ${producto.precio} </p>`
-//         conteiner.appendChild(div)
-//     });
+// .then(data => console.table(data))
 
-// })
+function obtenerProductos() {
+    fetch("../db/productos.json")
+    .then(resultado => resultado.json())
+    .then(data => {
+
+        data.forEach(producto => {
+
+            const div = document.createElement('div')
+            div.innerHTML = `<h3 id="${producto.id}"> ${producto.nombre} </h3>
+                            <p> ${producto.precio} </p>`
+        
+            conteiner.appendChild(div)
+        });
+
+    })
+}
+
+obtenerProductos()
+
