f65b68f6dbb47157e4f3c2c7d61d69172c3bd20e
Eduardo <eduardomoyla@gmail.com>
2024-09-12 16:37:58 -0400

    some styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 
+let conteiners = document.querySelectorAll(".card-body")
+
+function obtenerProductos() {
+    fetch("db/productos.json")
+    .then(resultado => resultado.json())
+    .then(data => {
+
+            console.log(data)
+
+        data.forEach((producto, i)=> {
+                const conteiner = conteiners[i]
+                const contenidoCard = document.createElement('div')
+                contenidoCard.className = "card-text-index"
+                contenidoCard.innerHTML = `<h3 class= "name-product-index"> ${producto.nombre} </h3>
+                                            <p class="price"> ${producto.precio} </p>`
+            
+                conteiner.appendChild(contenidoCard)
+
+        })
+    })
+}
+
+obtenerProductos()
