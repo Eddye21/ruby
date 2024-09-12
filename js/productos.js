f2194684e1afe0c0af47117e45265b4604e92365
Eduardo <eduardomoyla@gmail.com>
2024-09-12 09:57:45 -0400

    text cards solve

--- js/productos.js
+++ js/productos.js
@@ -1,22 +1,49 @@
-let conteiners = document.querySelectorAll(".text-card")
+let conteiners = document.querySelectorAll(".card-body")
 
 function obtenerProductos() {
     fetch("../db/productos.json")
     .then(resultado => resultado.json())
     .then(data => {
 
-        data.forEach(producto => {
-        conteiners.forEach(conteiner => {
-            const contenidoCard = document.createElement('div')
-            contenidoCard.innerHTML = `<h3> ${producto.nombre} </h3>
-                                        <p> ${producto.precio} </p>`
-        
-            conteiner.appendChild(contenidoCard)
-        })
-        });
 
+
+        data.forEach((producto, i)=> {
+            console.log(producto.id)
+                const conteiner = conteiners[i]
+                const contenidoCard = document.createElement('div')
+                contenidoCard.className = "card"
+                contenidoCard.innerHTML = `<h3> ${producto.nombre} </h3>
+                                            <p> ${producto.precio} </p>
+                                            <button id="${producto.id}" class="btn" > Agregar al carrito </button>`
+            
+                conteiner.appendChild(contenidoCard)
+        })
     })
 }
 
 obtenerProductos()
+
+        // data.forEach(producto => {
+        //     conteiners.forEach(conteiner => {
+        //         const contenidoCard = document.createElement('div')
+        //         contenidoCard.className = "card"
+        //         contenidoCard.innerHTML = `<h3> ${producto.nombre} </h3>
+        //                                     <p> ${producto.precio} </p>
+        //                                     <button id = "${producto.id}" class = "btn" > Agregar al carrito </button>`
+            
+        //         conteiner.appendChild(contenidoCard)
+        //     })
+        // });
+
+        // data.forEach(producto, i => {
+
+        //     const conteiner = conteiners[i]
+        //     const contenidoCard = document.createElement('div')
+        //     contenidoCard.className = "card"
+        //     contenidoCard.innerHTML = `<h3> ${producto.nombre} </h3>
+        //                                     <p> ${producto.precio} </p>
+        //                                     <button id="${producto.id}" class="btn" > Agregar al carrito </button>`
+            
+        //     conteiner.appendChild(contenidoCard)
+        // })
 
