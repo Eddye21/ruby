19a61b79e1930e5a1f007056066ffddaa5b1065d
Eduardo <eduardomoyla@gmail.com>
2024-09-12 11:02:07 -0400

    add toastify

--- js/productos.js
+++ js/productos.js
@@ -1,14 +1,12 @@
 let conteiners = document.querySelectorAll(".card-body")
-
 function obtenerProductos() {
     fetch("../db/productos.json")
     .then(resultado => resultado.json())
     .then(data => {
 
-
+            
 
         data.forEach((producto, i)=> {
-            console.log(producto.id)
                 const conteiner = conteiners[i]
                 const contenidoCard = document.createElement('div')
                 contenidoCard.className = "card-text"
@@ -17,9 +15,45 @@ function obtenerProductos() {
                                             <button id="${producto.id}" class="btn" > Agregar al carrito </button>`
             
                 conteiner.appendChild(contenidoCard)
+            agregarCarrito()
         })
     })
 }
+
+
+function agregarCarrito() {
+        let botonAgregar = document.querySelectorAll(".btn")
+        botonAgregar.forEach((boton)=> {
+        boton.onclick = () => {
+            notificacionToast()
+        }
+    })
+}
+
+function notificacionToast() {
+    Toastify({
+        text: "Producto agregado con exito",
+        duration: 3000,
+        style: {
+            background: "rgb(0,176,155)",
+        }
+    }).showToast();
+}
+
+
+
+
+// function agregarCarrito() {
+//     let agregar = document.getElementById("btn")
+//     console.log(agregar)
+//     agregar.forEach(boton => {
+//         boton.addEventListener("click", () => {
+//             notificacionToast()
+//         }) 
+// //     }); 
+// }
+
+// agregarCarrito()
 
 obtenerProductos()
 
