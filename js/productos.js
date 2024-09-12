010b3bd04b98c8efb94aa34ced6f3bdae3cba88f
Eduardo <eduardomoyla@gmail.com>
2024-09-12 11:19:06 -0400

    clear code

--- js/productos.js
+++ js/productos.js
@@ -38,46 +38,6 @@ function notificacionToast() {
             background: "rgb(0,176,155)",
         }
     }).showToast();
-}
-
-
-
-
-// function agregarCarrito() {
-//     let agregar = document.getElementById("btn")
-//     console.log(agregar)
-//     agregar.forEach(boton => {
-//         boton.addEventListener("click", () => {
-//             notificacionToast()
-//         }) 
-// //     }); 
-// }
-
-// agregarCarrito()
+}                       
 
 obtenerProductos()
-
-        // data.forEach(producto => {
-        //     conteiners.forEach(conteiner => {
-        //         const contenidoCard = document.createElement('div')
-        //         contenidoCard.className = "card"
-        //         contenidoCard.innerHTML = `<h3> ${producto.nombre} </h3>
-        //                                     <p> ${producto.precio} </p>
-        //                                     <button id = "${producto.id}" class = "btn" > Agregar al carrito </button>`
-            
-        //         conteiner.appendChild(contenidoCard)
-        //     })
-        // });
-
-        // data.forEach(producto, i => {
-
-        //     const conteiner = conteiners[i]
-        //     const contenidoCard = document.createElement('div')
-        //     contenidoCard.className = "card"
-        //     contenidoCard.innerHTML = `<h3> ${producto.nombre} </h3>
-        //                                     <p> ${producto.precio} </p>
-        //                                     <button id="${producto.id}" class="btn" > Agregar al carrito </button>`
-            
-        //     conteiner.appendChild(contenidoCard)
-        // })
-
